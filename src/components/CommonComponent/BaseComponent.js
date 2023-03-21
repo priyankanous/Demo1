7631b785c9564dcf8c93757838c9c8ef9071669f
Himanshu Mansharamani (BLR GSS) <himanshum@nousinfo.com>
2023-03-21 12:55:24 +0530

    [ADDED] Sub menu of Administration,[Modified] Route paths

--- src/components/CommonComponent/BaseComponent.js
+++ src/components/CommonComponent/BaseComponent.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { AiFillPlusSquare } from "react-icons/ai";
-import { TableHeadingSection,TableHeading,TableHeadingButton,TableHeadingButtonPlusIcon } from "../NavigationMenu/Value";
+import {
+  TableHeadingSection,
+  TableHeading,
+  TableHeadingButton,
+  TableHeadingButtonPlusIcon,
+} from "../NavigationMenu/Value";
 
 function BaseComponent(props) {
   return (
     <div className="table_container">
       <TableHeadingSection>
-                <TableHeading>Administration - {props.field}</TableHeading>
-                <TableHeadingButton onClick={props.setIsOpen}><TableHeadingButtonPlusIcon><AiFillPlusSquare></AiFillPlusSquare></TableHeadingButtonPlusIcon>{props.actionButtonName}</TableHeadingButton>
+        <TableHeading>Administration - {props.field}</TableHeading>
+        <TableHeadingButton onClick={props.setIsOpen}>
+          <TableHeadingButtonPlusIcon>
+            <AiFillPlusSquare></AiFillPlusSquare>
+          </TableHeadingButtonPlusIcon>
+          {props.actionButtonName}
+        </TableHeadingButton>
       </TableHeadingSection>
       <table>
         <tr>
@@ -15,6 +25,7 @@ function BaseComponent(props) {
           <th>{props.secondHeader}</th>
           <th>{props.thirdHeader}</th>
           <th>{props.fourthHeader}</th>
+          <th>{props.fifthHeader}</th>
         </tr>
         <tbody>
           {props.data && props.data.map((obj, id) => props.Tr({ ...obj }))}
